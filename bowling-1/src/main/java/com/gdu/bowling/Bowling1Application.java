0c0f26510e209481853c05e7e0a16616b3b8d8e9
gd <gd@DESKTOP-5MPQM31>
2020-07-27 17:59:12 +0900

    [Bowling]볼링프로그램 -gitHub test

---
+++ bowling-1/src/main/java/com/gdu/bowling/Bowling1Application.java
@@ -0,0 +1,13 @@
+package com.gdu.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bowling1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bowling1Application.class, args);
+	}
+
+}
---
+++ bowling-1/src/test/java/com/gdu/bowling/Bowling1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.bowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bowling1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
