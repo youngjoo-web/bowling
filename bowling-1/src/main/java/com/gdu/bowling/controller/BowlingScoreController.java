09e93ae55916242ce38d31c4b8756b84359ff6de
gd <gd@DESKTOP-5MPQM31>
2020-07-29 13:54:46 +0900

    [bowling]데이터베이스 연결 구현 - 볼링 게임화면에 데이터베이스의 데이터 출력 구현

--- bowling-1/src/main/java/com/gdu/bowling/controller/BowlingScoreController.java
+++ bowling-1/src/main/java/com/gdu/bowling/controller/BowlingScoreController.java
@@ -2,11 +2,12 @@ package com.gdu.bowling.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gdu.bowling.mapper.BowlingScoreMapper;
 import com.gdu.bowling.vo.BowlingScore;
@@ -16,11 +17,11 @@ public class BowlingScoreController {
 	@Autowired
 	private BowlingScoreMapper bowlingScoreMapper;
 	
-	@GetMapping("/index")
+	@GetMapping({"/", "/index"})
 	public String index(Model model) {
 		List<BowlingScore>list = bowlingScoreMapper.selectBowlingScoreList();
 		model.addAttribute("list",list);
-		return "index";
+		return "index/index";
 	}
 	
 }
